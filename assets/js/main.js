78b6aac1c68cffee3e79f165adc4fa082ebe43e4
aniket <aniket.umbc@gmail.com>
2023-04-19 13:10:24 +0530

    home section completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,15 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50
+    ? header.classList.add('bg-header')
+    : header.classList.remove('bg-header');
+};
+window.addEventListener('scroll', scrollHeader);
+
 /*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
